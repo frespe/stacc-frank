6a9adca177a4e2b2ab8dc562ea49c74dd877972f
Frank Rune Espeseth <Frank.Espeseth@student.uib.no>
2022-03-23 23:09:59 +0100

    Hentet highrisk-land fra ID, samt fikset input(?)

--- PEP.py
+++ PEP.py
@@ -1,5 +1,7 @@
 import csv
+import json
 from collections import Counter
+
 
 def open_file():
     with open("pep_small.csv", "r", encoding="utf-8") as data_file:
@@ -7,19 +9,36 @@ def open_file():
         elements = list(data)
         return elements
 
+
 data = open_file()
 
 
-
 def pep_check(files):
-    politicalParties = ['CIA World Leaders', 'CIA World Leaders;Every Politician', 'Every Politician;Members of the European Parliament', 'Members of the CoE Parliamentary Assembly;Members of the European Parliament', 'Every Politician', 'CIA World Leaders;Members of the CoE Parliamentary Assembly', 'Members of the CoE Parliamentary Assembly', 'CIA World Leaders;Members of the European Parliament', 'Every Politician;Members of the CoE Parliamentary Assembly;Members of the European Parliament', 'Members of the European Commitee of the Regions', 'Members of the European Parliament']
-  
+    politicalParties = []
     PEP = False
-    for i in politicalParties:
-        if files["dataset"] in i:
-            PEP = True
+
+    if files["dataset"] not in politicalParties:
+        politicalParties.append(files["dataset"])
+        PEP = True
     return PEP
 
+def get_organisation(files):
+    #high risk countries
+    hrc = ["Albania Barbados","Burkina Faso","Cambodia","Cayman Islands","Democratic People's Republic of Korea (DPRK)","Haiti","Iran","Jamaica","Jordan","Mali","Malta","Morocco","Myanmar","Nicaragua","Pakistan","Panama","Philippines","Senegal","South Sudan","Syria","Turkey","Uganda","Yemen","Zimbabwe"]
+    newHrc = []
+    for i in hrc:
+        newHrc.append(i.lower())
+    for org in files:
+        pepCompanies = []
+        org = org["id"].split("-")
+        for i in org:
+            if i in newHrc:
+                pepCompanies.append(i)
+    print(pepCompanies)
+
+    #return pepCompanies
+
+get_organisation(data)
 
 def get_sanctions(files):
     sanctions = False
@@ -28,31 +47,29 @@ def get_sanctions(files):
 
     return sanctions
 
-def high_risk_countries():
-    return None
-
 def check_person_for_sanctions(data):
-    inp = input("Enter name to check the sanction")
+    inp = input("Enter name to check if there are any sanctions")
     for persons in data:
         if inp == persons["name"]:
             checkSanction = get_sanctions(persons)
             if checkSanction:
                 print([f'{persons["name"]} Has not any sanctions'])
             else:
-                print([f'{persons["name"]} has the following sanctions: {persons["sanctions"]}'])
+                print(
+                    [f'{persons["name"]} has the following sanctions: {persons["sanctions"]}, please further investigate'])
+
 #check_person_for_sanctions(data)
 
 def check_person_for_PEP(data):
-    #inp = input("Enter name to check if PEP")
+    inp = input("Enter name to check if PEP")
     for persons in data:
-        #if inp == persons["name"]:
-        checkPEP = pep_check(persons)
-        if not checkPEP:
-            print([f'{persons["name"]} Is not a PEP'])
-        else:
-            print([f'{persons["name"]} is: {persons["dataset"]}'])
-check_person_for_PEP(data)
-
+        if inp == persons["name"]:
+            checkPEP = pep_check(persons)
+            if not checkPEP:
+                print([f'{persons["name"]} Is not a PEP'])
+            else:
+                print([f'{persons["name"]} is PEP: {persons["dataset"]}, please further investigate'])
+#check_person_for_PEP(data)
 
 def KYC():
     return None
@@ -66,5 +83,3 @@ def KYC():
 
 #     halla = input("Please enter a name or organisation")
 #     check_person_for_sanctions(data,halla)
-
-
